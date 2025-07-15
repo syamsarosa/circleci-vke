6380c521caf28924bf054a9209d21a7f3fad869a
ssarosa <syamsarosa@gmail.com>
2025-07-15 22:56:46 +0700

    Test auto deployment

--- index.php
+++ index.php
@@ -1,9 +1,13 @@
+<!DOCTYPE html>
 <html>
-<head>
-  <title>Vultr Demo</title>
-</head>
-<body>
-  <h1>Hello from Vultrr</h1>
-</body>
-</html>
 
+<head>
+    <title>Vultr Demo</title>
+</head>
+
+<body>
+    <h1><?php echo "Hello from Vultr"; ?></h1>
+    <p>This is just a test</p>
+</body>
+
+</html>
