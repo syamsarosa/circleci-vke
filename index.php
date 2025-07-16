5e6b333f897c55e26d7f505e00ecd01f0a7e954a
ssarosa <syamsarosa@gmail.com>
2025-07-17 02:46:21 +0700

    CircleCI | Test auto deployment with

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
     <h1><?php echo "Hello from Vultr"; ?></h1>
     <p>This is just a test</p>
     <p>Change CircleCI KUBECONFIG_BASE64 to 1 line</p>
+    <p>Test autodeployment using $CIRCLE_SHA1</p>
 </body>
 
 </html>
